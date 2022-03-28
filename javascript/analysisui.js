4db04624ba1f5effa6de44ade47a2209ef257d58
isaeb <isaacebert1@gmail.com>
2022-03-27 19:23:18 -0500

    added stuff

--- javascript/analysisui.js
+++ javascript/analysisui.js
@@ -1,5 +1,5 @@
 //Defining Variables
-var canvasWidth=200;
+var canvasWidth=800;
 var canvasHeight=600;
 var gameState=0;
 var interval=setInterval(updateCanvas,20);
@@ -11,12 +11,17 @@ var lineWidth = 3;
 var lineColor = 'grey';
 var mouseO = false;
 var moveSelect = 1;
+var board;
+var pieces;
+var spaceSize=75;
 
-function startGame(){
+function startUI(){
     gameCanvas.start();
-    desc = new createDescription(0, 0, 200, 80);
+    board=new createBoard();
+    pieces=new createPieces();
+    desc = new createDescription(600, 0, 200, 80);
     bg = new createBackground();
-    notation = new createNotation(0, 80);
+    notation = new createNotation(600, 80);
     gameCanvas.canvas.addEventListener('wheel', scrl);
 }
 
@@ -31,6 +36,191 @@ var gameCanvas={
         this.context=this.canvas.getContext("2d",{alpha:false});
         document.body.insertBefore(this.canvas,document.body.childNodes[0]);
         
+    }
+}
+
+function createBoard(){
+    this.draw=function(){
+        ctx=gameCanvas.context;
+        //draw the dark squares
+        ctx.fillStyle = '#e28743';
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        ctx.fillStyle = '#eab676';
+        for(let i=0;i<8;i++){
+            for(let j=0;j<8;j++){
+                if((i+j)%2==0){ctx.fillRect(i*spaceSize,j*spaceSize,spaceSize,spaceSize);}
+            }
+        }
+    }
+}
+
+function createPieces(){
+    //creating the white pieces
+    this.whitePieces=[];
+
+    //pawns
+    for(let i=0;i<8;i++){
+        this.whitePieces.push(new createPawn(i+1,2,'white'));
+    }
+
+    //knights
+    this.whitePieces.push(new createKnight(2,1,'white'));
+    this.whitePieces.push(new createKnight(7,1,'white'));
+
+    //bishops
+    this.whitePieces.push(new createBishop(3,1,'white'));
+    this.whitePieces.push(new createBishop(6,1,'white'));
+
+    //rooks
+    this.whitePieces.push(new createRook(1,1,'white'));
+    this.whitePieces.push(new createRook(8,1,'white'));
+
+    //queen
+    this.whitePieces.push(new createQueen(4,1,'white'));
+
+    //king
+    this.whitePieces.push(new createKing(5,1,'white'));
+
+    //creating the black pieces
+    this.blackPieces=[];
+
+    //pawns
+    for(let i=0;i<8;i++){
+        this.blackPieces.push(new createPawn(i+1,7,'black'));
+    }
+
+    //knights
+    this.blackPieces.push(new createKnight(2,8,'black'));
+    this.blackPieces.push(new createKnight(7,8,'black'));
+
+    //bishops
+    this.blackPieces.push(new createBishop(3,8,'black'));
+    this.blackPieces.push(new createBishop(6,8,'black'));
+
+    //rooks
+    this.blackPieces.push(new createRook(1,8,'black'));
+    this.blackPieces.push(new createRook(8,8,'black'));
+
+    //queen
+    this.blackPieces.push(new createQueen(4,8,'black'));
+
+    //king
+    this.blackPieces.push(new createKing(5,8,'black'));
+
+
+    this.draw=function(){
+        for(let i=0;i<this.whitePieces.length;i++){
+            this.whitePieces[i].draw();
+            this.blackPieces[i].draw();
+        }
+    }
+}
+
+function createPawn(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_plt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_pdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createKnight(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_nlt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_ndt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createBishop(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_blt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_bdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createRook(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_rlt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_rdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createQueen(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_qlt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_qdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createKing(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_klt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_kdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
     }
 }
 
@@ -103,10 +293,10 @@ function createBackground(){
         ctx = gameCanvas.context;
 
         ctx.fillStyle = lineColor;
-        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        ctx.fillRect(600, 0, canvasWidth, canvasHeight);
 
         ctx.fillStyle = '#181818';
-        ctx.fillRect(lineWidth, lineWidth, canvasWidth - lineWidth * 2, canvasHeight - lineWidth * 2);
+        ctx.fillRect(600+lineWidth, lineWidth, canvasWidth - lineWidth * 2, canvasHeight - lineWidth * 2);
     }
 }
 
@@ -150,7 +340,7 @@ function createDescription(x, y, width, height){
         ctx.fillText("Engine evaluation: " + this.eval, x + 10, y + 60);
         //cutoff
         ctx.fillStyle = lineColor;
-        ctx.fillRect(this.width - lineWidth, this.y, lineWidth, this.height);
+        ctx.fillRect(this.width - lineWidth+600, this.y, lineWidth, this.height);
 
         
     }
@@ -159,6 +349,8 @@ function createDescription(x, y, width, height){
 function updateCanvas(){
     switch(gameState){
         case 0:
+            board.draw();
+            pieces.draw();
             bg.draw();
             desc.draw();
             notation.draw();
--- javascript/gameui.js
+++ javascript/gameui.js
@@ -1,5 +1,5 @@
 //Defining Variables
-var canvasWidth=200;
+var canvasWidth=800;
 var canvasHeight=600;
 var gameState=0;
 var interval=setInterval(updateCanvas,20);
@@ -11,16 +11,205 @@ var lineWidth = 3;
 var lineColor = 'grey';
 var mouseO = false;
 var moveCount = 0;
+var board;
+var pieces;
+var spaceSize=75;
 
-function startGame(){
+function startUI(){
     gameCanvas.start();
-    timer = new createTimer(0, 0, 200, 100, 10, 2);
+    board=new createBoard();
+    pieces=new createPieces();
+    timer = new createTimer(600, 0, 200, 100, 10, 2);
     bg = new createBackground();
-    notation = new createNotation(0, 100);
+    notation = new createNotation(600, 100);
     gameCanvas.canvas.addEventListener('wheel', scrl);
+    console.log("Yoooo");
 }
 
+function createBoard(){
+    this.draw=function(){
+        ctx=gameCanvas.context;
+        //draw the dark squares
+        ctx.fillStyle = '#e28743';
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        ctx.fillStyle = '#eab676';
+        for(let i=0;i<8;i++){
+            for(let j=0;j<8;j++){
+                if((i+j)%2==0){ctx.fillRect(i*spaceSize,j*spaceSize,spaceSize,spaceSize);}
+            }
+        }
+    }
+}
 
+function createPieces(){
+    //creating the white pieces
+    this.whitePieces=[];
+
+    //pawns
+    for(let i=0;i<8;i++){
+        this.whitePieces.push(new createPawn(i+1,2,'white'));
+    }
+
+    //knights
+    this.whitePieces.push(new createKnight(2,1,'white'));
+    this.whitePieces.push(new createKnight(7,1,'white'));
+
+    //bishops
+    this.whitePieces.push(new createBishop(3,1,'white'));
+    this.whitePieces.push(new createBishop(6,1,'white'));
+
+    //rooks
+    this.whitePieces.push(new createRook(1,1,'white'));
+    this.whitePieces.push(new createRook(8,1,'white'));
+
+    //queen
+    this.whitePieces.push(new createQueen(4,1,'white'));
+
+    //king
+    this.whitePieces.push(new createKing(5,1,'white'));
+
+    //creating the black pieces
+    this.blackPieces=[];
+
+    //pawns
+    for(let i=0;i<8;i++){
+        this.blackPieces.push(new createPawn(i+1,7,'black'));
+    }
+
+    //knights
+    this.blackPieces.push(new createKnight(2,8,'black'));
+    this.blackPieces.push(new createKnight(7,8,'black'));
+
+    //bishops
+    this.blackPieces.push(new createBishop(3,8,'black'));
+    this.blackPieces.push(new createBishop(6,8,'black'));
+
+    //rooks
+    this.blackPieces.push(new createRook(1,8,'black'));
+    this.blackPieces.push(new createRook(8,8,'black'));
+
+    //queen
+    this.blackPieces.push(new createQueen(4,8,'black'));
+
+    //king
+    this.blackPieces.push(new createKing(5,8,'black'));
+
+
+    this.draw=function(){
+        for(let i=0;i<this.whitePieces.length;i++){
+            this.whitePieces[i].draw();
+            this.blackPieces[i].draw();
+        }
+    }
+}
+
+function createPawn(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_plt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_pdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createKnight(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_nlt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_ndt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createBishop(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_blt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_bdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createRook(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_rlt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_rdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createQueen(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_qlt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_qdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
+
+function createKing(x,y,color){
+    this.x=x;
+    this.y=y;
+    this.color=color;
+    this.realX=(x-1)*spaceSize;
+    this.realY=canvasHeight-y*spaceSize;
+    this.draw=function(){
+        let img = new Image();
+        if(color==='white'){
+            img.src='javascript/pieceIcons/white/Chess_klt60.png';
+        }else{
+            img.src='javascript/pieceIcons/black/Chess_kdt60.png';
+        }
+        ctx=gameCanvas.context;
+        ctx.drawImage(img,this.realX,this.realY,spaceSize,spaceSize);
+    }
+}
 
 var gameCanvas={
     canvas: document.createElement("canvas"),
@@ -31,6 +220,21 @@ var gameCanvas={
         this.context=this.canvas.getContext("2d",{alpha:false});
         document.body.insertBefore(this.canvas,document.body.childNodes[0]);
         
+    }
+}
+
+function createBoard(){
+    this.draw=function(){
+        ctx=gameCanvas.context;
+        //draw the dark squares
+        ctx.fillStyle = '#e28743';
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        ctx.fillStyle = '#eab676';
+        for(let i=0;i<8;i++){
+            for(let j=0;j<8;j++){
+                if((i+j)%2==0){ctx.fillRect(i*spaceSize,j*spaceSize,spaceSize,spaceSize);}
+            }
+        }
     }
 }
 
@@ -84,10 +288,10 @@ function createBackground(){
         ctx = gameCanvas.context;
 
         ctx.fillStyle = lineColor;
-        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        ctx.fillRect(600, 0, canvasWidth, canvasHeight);
 
         ctx.fillStyle = '#181818';
-        ctx.fillRect(lineWidth, lineWidth, canvasWidth - lineWidth * 2, canvasHeight - lineWidth * 2);
+        ctx.fillRect(600 + lineWidth, lineWidth, canvasWidth - lineWidth * 2, canvasHeight - lineWidth * 2);
     }
 }
 
@@ -125,6 +329,9 @@ function createTimer(x, y, width, height, time, increment){
 function updateCanvas(){
     switch(gameState){
         case 0:
+            
+            board.draw();
+            pieces.draw();
             bg.draw();
             timer.draw();
             notation.draw();
