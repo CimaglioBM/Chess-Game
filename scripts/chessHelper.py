53dfaa9b1a14bc5666a694026f6e0d775125cf53
isaeb <91221974+isaeb@users.noreply.github.com>
2022-05-03 15:18:57 -0500

    some improvements

---
+++ scripts/chessHelper.py
@@ -0,0 +1,34 @@
+import chess
+import random
+
+def moveLegal(fen, moveStart, moveEnd):
+    b = chess.Board(fen)
+    if(chess.Move.from_uci(moveStart+moveEnd) in b.legal_moves):
+        return "true"
+    else:
+        return "false"
+
+
+def getNewFen(fen, moveStart, moveEnd):
+    b = chess.Board(fen)
+    b.push_uci(moveStart+moveEnd)
+    return b.fen()
+
+def getNewFenSan(fen, san):
+    b = chess.Board(fen)
+    b.push_san(san)
+    return b.fen()
+
+
+def uciToAn(fen, moveStart, moveEnd):
+    b = chess.Board(fen)
+    return b.san(chess.Move.from_uci(moveStart+moveEnd))
+
+def getComputerMove(fen, depth):
+    b = chess.Board(fen)
+    legal_moves = str(b.legal_moves)[38:-2].replace(',','').split()
+    
+    move_index = random.randint(0,len(legal_moves))
+    print(legal_moves[0])
+    return legal_moves[0]
+    
